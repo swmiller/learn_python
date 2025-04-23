274807099cfa2d885e5b7c5ee91802642b8e8447
Stephen Miller <smiller@crowninformationservices.com>
2025-04-23 11:29:31 -0500

    Improve invalid input handler

--- Day 13/fix_the_errors.py
+++ Day 13/fix_the_errors.py
@@ -1,6 +1,10 @@
-age = int(input("How old are you?"))
-if age > 18:
-    # Print statement was not originally indented correctly
-    print(
-        f"You can drive at age {age}."
-    )  # Orignall printed "{age} instead of the number entered by the user. The "f" was missing.
+while True:
+    try:
+        age = int(input("How old are you? "))
+        if age > 18:
+            print(f"You can drive at age {age}.")
+        else:
+            print(f"You cannot drive at age {age}.")
+        break
+    except ValueError:
+        print("Please enter a valid number.")
